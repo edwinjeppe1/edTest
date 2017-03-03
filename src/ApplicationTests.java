b68e24bac4228306a910895af09e1ba672b4279f
edwinjeppe1 <jeppe.edwin@gmail.com>
2017-03-03 15:40:59 +0200

    Test files
    Test files

---
+++ src/ApplicationTests.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class ApplicationTests {
+
+	private Calculations _calc;
+	private int _valueA;
+	private int _valueB;
+	
+	
+	@Before
+	 public void InitializeCalculationVariables(){
+	    _calc = new Calculations();
+	    _valueA = 5;
+	    _valueB = 2;
+	  }
+	
+	@Test(timeout=1000)
+	public void Test_Calculations_Multiply_Pass() {
+		assertEquals("Should be 10", 10,_calc.Multiply(_valueA, _valueB));	
+	}
+
+	@Test(timeout=1000)
+	public void Test_Calculations_Multiply_Fail() {
+		assertNotEquals("Should not be 10", 11,_calc.Multiply(_valueA, _valueB));	
+	}
+	
+}
---
+++ src/Calculations.java
@@ -0,0 +1,8 @@
+
+public class Calculations {
+	
+	 public int Multiply(int a, int b){
+		    return a*b;
+		  }
+
+}
